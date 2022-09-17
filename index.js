1ddc3fc7ba3077c984d280f4c3eaf5fb26e0660e
Hiba <eng.lilil92@gmail.com>
2022-09-18 00:15:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const num3 = 10;
+const num4 = 6 ;
+const multiply = num1 * num2 ;
+console.log(multiply);
+
+/*W3School ==> Return a random number between 0 and 10:  let x = Math.random() * 10;*/
+const random = Math.floor(Math.random() * 100 ) + 1;  //return a random number between 0 and 100
+console.log(random);
+
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(1,5,10,15,20);
+console.log(max);
